cbf11b1ea3732d4a4a80ec65fa09d3cdb8c8ba4c
RomainCocogne <romain.cocogne83@gmail.com>
2019-02-28 19:38:55 +0100

    servo working with ultra sound

--- run_servo/run_servo.ino
+++ run_servo/run_servo.ino
@@ -3,24 +3,51 @@
 #include <Servo.h>
 
 #define COMMAND 9 
-#define RIGHT 5
+#define RIGHT 3
 #define LEFT 4
+
+#define R_TRIG 10
+#define R_ECHO 11
+#define L_TRIG 5
+#define L_ECHO 6
 
 Servo servo;
 
 uint8_t angle=90;
 
 void setup() {
+	pinMode(R_TRIG, OUTPUT);
+	pinMode(L_TRIG, OUTPUT);
+	pinMode(R_ECHO, INPUT);
+	pinMode(L_ECHO, INPUT);
+
   servo.attach(COMMAND);
   servo.write(angle);
   Serial.begin(9600);
 }
 
-void loop() {
-	int r=analogRead(RIGHT);
-	int l=analogRead(LEFT);
 
-	angle= map(l-r,1024,-1024,0,180);
+
+void loop() {
+	//int r=analogRead(RIGHT);
+	//int l=analogRead(LEFT);
+
+	digitalWrite(R_TRIG, LOW);
+	delayMicroseconds(2);
+	digitalWrite(R_TRIG, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(R_TRIG, LOW);
+	int r=pulseIn(R_ECHO, HIGH);
+
+	digitalWrite(L_TRIG, LOW);
+	delayMicroseconds(2);
+	digitalWrite(L_TRIG, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(L_TRIG, LOW);
+	int l=pulseIn(L_ECHO, HIGH);
+
+
+	angle= map(l-r,2000,-2000,0,180);
 	servo.write(angle);
 	// if (Serial.available() > 0) {
 	// 	adapte_angle(Serial.read());
