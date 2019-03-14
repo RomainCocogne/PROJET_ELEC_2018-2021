c9092d8dbc4cb14cd90bd28b197f43790536b46f
RomainCocogne <romain.cocogne83@gmail.com>
2019-03-14 17:11:37 +0100

    add command_car

---
+++ command_car.c
@@ -0,0 +1,74 @@
+/*
+	Code a mettre sur la raspberry
+
+	permet de commander la voiture a distance avec le clavier
+*/
+
+/************ library **********/
+
+#include <stdio.h >		#include <stdlib.h>
+#include <curses.h>		#include <errno.h>
+#include <wiringPi.h>	#include <wiringSerial.h>
+
+/*******************************/
+
+/*********** macro *************/
+
+#define FOR   22	#define BACK  23
+#define SPEED 18	#define ANGLE 12 
+
+/*******************************/
+
+/******* initialisation *******/
+
+//communication arduino
+void init_serial(int *serial_port){
+	if ((*serial_port = serialOpen ("/dev/ttyS0", 9600)) < 0)	/* open serial port */
+  {
+    fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+  }
+}
+
+//lib wiringPi
+void init_wiringPi(){
+	if (wiringPiSetupGpio () == -1)					/* initializes wiringPi setup */
+  {
+    fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+  }
+}
+
+//mode des pins
+void init_pin(){
+	pinMode(FOR,OUTPUT);
+	pinMode(BACK,OUTPUT);
+	pinMode(ANGLE,OUTPUT);
+	pinMode(SPEED,PWM_OUTPUT);
+}
+
+/*************************************/
+
+
+
+
+/**************** main ***************/
+
+int main (void){
+	int serial_port;
+	init_serial(&serial_port);
+	init_wiringPi();
+	initscr();
+
+	int c;
+	while(1){
+		if(c = getch()){
+			serialPuts(serial_port,c); //Envoie c par uart
+		}
+		delay(10);
+	}
+
+	return EXIT_SUCCESS;
+
+}
+
+/************************************/
+
---
+++ command_car/command_car.ino
@@ -0,0 +1,83 @@
+/*
+	Code a mettre sur l arduino
+
+	permet de commander la voiture a distance avec le clavier
+*/
+
+/************ library **********/
+
+#include <Servo.h>
+
+/*******************************/
+
+/*********** macro *************/
+
+//output
+#define FOR   7		#define BACK  8
+#define SPEED 9		#define SERVO 12 
+//input
+#define IR_R  3		#define IR_L  4
+
+/*******************************/
+
+/*********** variables *********/
+
+//pour le servo
+Servo servo;
+int angle=90;
+//pour le moteur
+int speed=0;
+/******************************/
+
+
+void setup() {
+//pin mode
+	//output 						//input pas utiles ici
+	pinMode (FOR  , OUTPUT);		pinMode (IR_R , INPUT );
+  	pinMode (BACK , OUTPUT);		pinMode (IR_L , INPUT );
+  	pinMode (SPEED, OUTPUT);
+
+//servo
+	servo.attach (COMMAND);			servo.write(angle);
+
+//serial
+	Serial.begin(9600); 			while(!Serial);
+	Serial.write("___init ok___ \nStart runing motor.....\n");
+}
+
+/****************************************/
+
+void loop() {
+	if (Serial.available() > 0) {
+		extract_data(Serial.read(),&speed,&angle);
+		command_car(speed,angle);
+  	}
+}
+
+/***************************************/
+
+/************* fonctions **************/
+
+//traite les donnees recus par le serial
+void extract_data(char c, int *speed, int *angle){
+	switch (c) {
+	    case 'z':		if(*speed<245 ) 	*speed=*speed+10;		break ;
+	    case 's':		if(*speed>-245)     *speed=*speed-10; 	    break ;
+	    case 'd':		if(*angle>0   ) 	*angle=*angle-10;		break ;
+	    case 'q':		if(*angle<180 ) 	*angle=*angle+10;		break ;
+	    case 'a':							*speed=0        ;		break ;
+	    case 'e':							*angle=90       ;		break ;
+	    default:													return;
+	}
+}
+
+/*************/
+
+//fait rouler la voiture
+void command_car(int speed, int angle){
+	if      (speed>0)		{digitalWrite(FOR, HIGH);		digitalWrite(BACK, LOW );}
+	else if (speed<0)		{digitalWrite(FOR, LOW );	    digitalWrite(BACK, HIGH);
+															speed = -speed          ;}
+	analogWrite(SPEED, speed);
+	servo.write(angle);
+}
